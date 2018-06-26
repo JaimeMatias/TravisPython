3f24bc4ff76af24e1b1ffbcd33f7542e2e52daea
JaimeMatias <matias.raul.jaime@gmail.com>
2018-06-26 11:53:55 -0300

    Cambio apariencia

--- webapp.py
+++ webapp.py
@@ -9,6 +9,7 @@ HTML = """
     <head>
         <title>Home | TravisCI</title>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="index3.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="description" content="Social media analytic tool">
